eafc80ea79d27375b46230bc9aa4f14019c2eadf
Ogaga <iyaraogaga69@gmail.com>
2022-10-20 13:51:08 +0100

    Select all needed html elements

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const refresh = document.querySelector(".fa-arrows-rotate");
+const textInput = document.querySelector('.text-input')
+let checkbox 
+let listInput 
+let deleteInput
+const deleteBtn = document.querySelector('.submit')
+const listContainer = document.querySelector(".todo");
